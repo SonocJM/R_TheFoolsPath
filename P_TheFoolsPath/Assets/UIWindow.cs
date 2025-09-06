846334366896016cbef8253a3a604f64df405685
SonocJM <sonxzisulishimada@gmail.com>
2025-09-05 20:18:19 -0600

    Adding Animation UI

---
+++ P_TheFoolsPath/Assets/UIWindow.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using Unity.Collections;
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    [SerializeField] private string _windowID;
+    [SerializeField] private Canvas _windowCanvas;
+    [SerializeField] private CanvasGroup _windowCanvasGroup;
+
+    [SerializeField] private bool hideOnStart = true;
+    [SerializeField] private float animationTime = 0.5f;
+
+
+    public string WindowUI => _windowID;
+
+    public void Start()
+    {
+        Initialize();
+    }
+    public virtual void Initialize()
+    {
+        if (hideOnStart) Hide(instant: true);
+    }
+
+    public virtual void Show(bool instant = false)
+    {
+        //_windowCanvas.gameObject.SetActive(true);
+        if (instant)
+        {
+            _windowCanvasGroup.transform.DOScale(endValue: Vector3.one, duration: 0f);
+        }
+        else
+        {
+            _windowCanvasGroup.transform.DOScale(endValue: Vector3.one, animationTime);
+        }
+    }
+
+    public virtual void Hide(bool instant = false)
+    {
+        //_windowCanvas.gameObject.SetActive(false);
+
+        if (instant)
+        {
+            _windowCanvasGroup.transform.DOScale(endValue: Vector3.one, duration: 0f);
+        }
+        else
+        {
+            _windowCanvasGroup.transform.DOScale(endValue: Vector3.one, animationTime);
+        }
+    }
+}
