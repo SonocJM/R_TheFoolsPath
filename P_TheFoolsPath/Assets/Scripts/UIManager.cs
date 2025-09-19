7950053295ca35ff29d37be55a6f71a354bf51d3
Angry6irds <sonxzisulishimada@gmail.com>
2025-09-18 20:48:42 -0600

    edititng 1.1

--- P_TheFoolsPath/Assets/Scripts/UIManager.cs
+++ P_TheFoolsPath/Assets/Scripts/UIManager.cs
@@ -1,6 +1,91 @@
+using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
+using Dino.UtilityTools.Singleton;
 
-public class UIManager
+public class UIManager : Singleton<UIManager>
 {
+    // List of all UI windows 
+    [SerializeField] private List<UIWindow> uiWindows = new List<UIWindow>();
     
+    /// <summary>
+    ///  Shows the UI window with the specified identifier.
+    /// </summary>
+    /// <param name="windowUI"> Window identifier</param>
+    public void ShowUI(string windowUI)
+    {
+        //foreach (var window in uiWindows)
+        //{
+        //    if (window.WindowID == windowUI)
+        //    {
+        //        window.Show();
+        //        return;
+        //    }
+        //}
+        //Debug.LogWarning($"UI Window with name {windowUI} not found.");
+    }
+    
+    /// <summary>
+    ///  Hides the UI window with the specified identifier.
+    /// </summary>
+    /// <param name="windowUI"> Window identifier</param>
+    public void HideUI(string windowUI)
+    {
+        //foreach (var window in uiWindows)
+        //{
+        //    if (window.WindowID == windowUI)
+        //    {
+        //        window.Hide();
+        //        return;
+        //    }
+        //}
+        //Debug.LogWarning($"UI Window with name {windowUI} not found.");
+    }
+    
+    /// <summary>
+    /// Hides all UI windows.
+    /// </summary>
+    public void HideAllUI()
+    {
+        foreach (var window in uiWindows)
+        {
+            window.Hide();
+        }
+    }
+
+    /// <summary>
+    ///  Retrieves the UI window with the specified identifier.
+    /// </summary>
+    /// <param name="windowUI"></param>
+    /// <returns></returns>
+    //public UIWindow GetUIWindow(string windowUI)
+    //{
+    //    //foreach (var window in uiWindows)
+    //    //{
+    //    //    if (window.WindowID == windowUI)
+    //    //    {
+    //    //        return window;
+    //    //    }
+    //    //}
+    //    //Debug.LogWarning($"UI Window with name {windowUI} not found.");
+    //    //return null;
+    //}
+
+    #region Editor
+    [Button]
+    private void GetAllUIWindows()
+    {
+        //uiWindows.Clear();
+        //UIWindow[] windows = FindObjectsByType<UIWindow>(FindObjectsSortMode.InstanceID);
+        //uiWindows.AddRange(windows);
+    }
+
+    #endregion
+    
+}
+
+public static class WindowsIDs
+{
+    public static string Popup = "PopupUI";
+
 }
